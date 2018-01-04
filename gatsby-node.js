002844891318f973ed44b48e7e5723068eb744cb
Tiffany Lam <tlam5@sbcglobal.net>
2018-01-04 12:26:14 -0800

    Include sort, limit, excerpt, and date in createPages query

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,12 +14,17 @@
    const postTemplate = path.resolve('src/templates/post.js');
 
    return graphql(`{
-     allMarkdownRemark {
+     allMarkdownRemark(
+       sort: { order: DESC, fields: [frontmatter___date] }
+       limit: 1000
+     ) {
        edges {
          node {
+           excerpt(pruneLength: 250)
            html
            id
            frontmatter {
+             date
              path
              title
            }
